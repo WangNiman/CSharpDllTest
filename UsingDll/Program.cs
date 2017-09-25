2ac28c90d965e284ff5c21b9e6e47d6a65532fac
WangNiman <2467853836@qq.com>
2017-09-25 15:09:45 +0800

    初始化

---
+++ UsingDll/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UsingDll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassLibrary.Class1 Jack = new ClassLibrary.Class1("Jack");
+            Jack.sayHello();
+            Console.WriteLine("using successful!");
+            Console.ReadKey();
+        }
+    }
+}
